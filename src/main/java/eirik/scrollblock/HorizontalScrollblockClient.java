1504d0ed1b73e22b3ac963e93495f348eb9733a8
Eirik Nilsen <eiriknils1@gmail.com>
2024-02-18 15:17:50 +0100

    Initial commit

---
+++ src/main/java/eirik/scrollblock/HorizontalScrollblockClient.java
@@ -0,0 +1,11 @@
+package eirik.scrollblock;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HorizontalScrollblockClient  implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/eirik/scrollblock/HorizontalScrollblockDataGenerator.java
@@ -0,0 +1,11 @@
+package eirik.scrollblock;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HorizontalScrollblockDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
